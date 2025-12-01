dc029fabd8ed87e28c92a9ebea7ba36c1f3510d1
huynhnguyenthuongtin <tin.hnt.64cntt@ntu.edu.vn>
2025-12-01 19:49:38 +0700

    code

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('../config.php');
+include('../config.php'); 
 
 if (!isset($_SESSION['username']) || !isset($_SESSION['level'])) {
     header('Location: dang-nhap.php');
